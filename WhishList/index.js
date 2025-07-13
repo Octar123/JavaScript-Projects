92162e1f1b7e1b0333880f19ea8efdc029c2ffc7
Octar123 <ranjanassmali123@gmail.com>
2025-07-13 13:57:24 +0530

    solved the comma showing task and local storage is remaining

--- WhishList/index.js
+++ WhishList/index.js
@@ -13,16 +13,26 @@ function uuid() {
     });
 }
 
+todosContainerEle.addEventListener("click", (e) => {
+    let key = e.target.dataset.key;
+    let deleteKey = e.target.dataset.todokey;
+    todoList = todoList.map(todo => todo.id === key ? {...todo, isCompleted: !todo.isCompleted} : todo);
+    todoList = todoList.filter(todo => todo.id !== deleteKey);
+    displayTodoList(todoList);
+})
+
 buttonEle.addEventListener("click", (e) => {
     e.preventDefault();
     todo = inputEle.value;
     if(todo.length > 0){
         todoList.push({id: uuid(), todo, isCompleted: false});
     }
-    console.log(todoList)
+
     displayTodoList(todoList);
+    inputEle.value = "";
 })  
 
+
 function displayTodoList(todoList){
-    todosContainerEle.innerHTML = todoList.map(({id, todo, isCompleted}) => `<div><input id="item-${id}" type="checkbox"><label for="item-${id}" class="todo">${todo}</label><button>Delete</button></div>`)
+    todosContainerEle.innerHTML = todoList.map(({id, todo, isCompleted}) => `<div><input data-key=${id} id="item-${id}" type="checkbox" ${isCompleted ? "checked" : ""}><label data-key=${id} for="item-${id}" class="todo todo-text t-pointer ${isCompleted ? "checked-todo" : ""}">${todo}</label><button class="button cursor" data-todokey=${id}>Delete</button></div>`).join('');
 }
